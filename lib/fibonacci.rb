d0b75ff532eef397990a43bb5027d1f33c33b8e9
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-09 12:49:45 -0700

    can calculate number based off of fibonacci_number

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,8 +5,21 @@
 # ....
 # e.g. 6th fibonacci number is 8
 
-# Time complexity: ?
-# Space complexity: ?
-def fibonacci(n)
-  raise NotImplementedError
+# Time complexity: O(n) where n is the value of the fibonacci_number
+# Space complexity: O(1)
+def fibonacci(fibonacci_number)
+  raise ArgumentError if fibonacci_number == nil || fibonacci_number < 0
+  return 0 if fibonacci_number == 0
+
+  i = 0
+  first_number = 0
+  second_number = 1
+
+  while i < (fibonacci_number - 1)
+    temp = second_number
+    second_number = first_number + second_number
+    first_number = temp
+    i += 1
+  end
+  return second_number
 end
